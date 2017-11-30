b42679b122e71196ca45343f499c49cf0d19c469
Daniel <daniel@dconnolly.com>
2017-11-30 11:59:56 +0100

    change currency to BCH from BCC

--- core/src/main/java/org/bitcoinj/utils/MonetaryFormat.java
+++ core/src/main/java/org/bitcoinj/utils/MonetaryFormat.java
@@ -54,11 +54,11 @@ public final class MonetaryFormat {
     /** Standard format for fiat amounts. */
     public static final MonetaryFormat FIAT = new MonetaryFormat().shift(0).minDecimals(2).repeatOptionalDecimals(2, 1);
     /** Currency code for base 1 Bitcoin. */
-    public static final String CODE_BTC = "BCC";
+    public static final String CODE_BTC = "BCH";
     /** Currency code for base 1/1000 Bitcoin. */
-    public static final String CODE_MBTC = "mBCC";
+    public static final String CODE_MBTC = "mBCH";
     /** Currency code for base 1/1000000 Bitcoin. */
-    public static final String CODE_UBTC = "µBCC";
+    public static final String CODE_UBTC = "µBCH";
 
     public static final int MAX_DECIMALS = 8;
 
